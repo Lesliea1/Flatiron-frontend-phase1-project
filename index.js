85f7e57e227ec798e1e593d774836b2e66860d7b
leslie <alvarado.leslie12@yahoo.com>
2023-07-01 15:18:37 -0700

    custom css

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log ('js connected 2.0')
+
+
+const handleFormInputFocus = () => {
+console.log("it works!")
+}
+
+const searchTermsInput = document.body.querySelector("#search-terms")
+
+searchTermsInput.addEventListener("focus", handleFormInputFocus);
