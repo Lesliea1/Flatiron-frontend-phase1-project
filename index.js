0f9d1f49791485fc37cc3908a5c5595e82402899
leslie <alvarado.leslie12@yahoo.com>
2023-07-03 21:23:56 -0700

    selector dropdown updated

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ getCocktails();
 function getCocktails() {
   fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
     .then((r) => r.json())
-    .then((cocktails) => console.log()) //renderCocktailOptions(cocktails.drinks))
+    .then((cocktails) => renderCocktailOptions(cocktails.drinks))
     .catch();
+}
+
+function renderCocktailOptions(cocktails) {
+  cocktails.forEach((cocktail) => {
+    const option = document.createElement("option");
+    option.value = cocktail.strDrink;
+    option.textContent = cocktail.strDrink;
+    drinkSelect.append(option);
+  });
 }
