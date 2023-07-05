44b5aa4d45d79a36867d77ff586ebf896902c8fa
leslie <alvarado.leslie12@yahoo.com>
2023-07-05 14:37:24 -0700

    added functions update 2-3

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// Elements
 const drinkSelect = document.querySelector("#cocktails");
 const categorySelect = document.querySelector("#categories");
+const cocktailContainer = document.querySelector(".cocktail-container");
 
 // Function calls
 getACocktails();
 getNACocktails();
 
 // Event Listeners
-drinkSelect.addEventListener("change", getDrink);
-categorySelect.addEventListener("change", getCocktailByCategory);
+drinkSelect.addEventListener("change", getCocktail);
+categorySelect.addEventListener("change", getNADrink);
 
+//Dropdown Functiond
 function getACocktails() {
   fetch(
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink"
@@ -43,4 +46,50 @@ function renderCategoryOptions(categories) {
     option.textContent = category.strDrink;
     categorySelect.append(option);
   });
+}
+
+// Card Functions
+
+function getCocktail() {
+  const aCocktail = e.target.value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${aCocktail}`)
+    .then((r) => r.json())
+    .then((alchoholic) => renderAllCocktails(alchoholic.drinks))
+    .catch((error) => alert(error));
+}
+
+function getNADrink() {
+  const nADrink = e.target.value;
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${nADrink}`)
+    .then((r) => r.json())
+    .then((nonAlcoholic) => renderAllNAlcoholic(nonAlcoholic.drinks))
+    .catch((error) => alert(error));
+}
+
+function renderAllCocktails(aDrinks) {
+  aDrinks.forEach((aDrink) => {
+    renderCocktailCard(aDrink);
+  });
+  drinkSelect.value = "";
+  categorySelect.value = "";
+}
+
+function renderCocktailCard(aDrink) {
+  const {
+    idDrink: cocktailId,
+    strDrinkThumb: cocktailImage,
+    strDrink: cocktailName,
+  } = aDrink;
+
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+  //add event listener to card
+}
+
+function renderAllNAlcoholic(NDrinks) {
+  NDrinks.forEach((nDrink) => {});
+  drinkSelect.value = "";
+  categorySelect.value = "";
 }
